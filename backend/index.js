48967255565607747749ce1f2bcf68ceb40f8e9f
Nelith Karunarathne <nelith550@gmail.com>
2024-12-19 01:21:32 +0530

    Update functions and selecting fuctions were created

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,8 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to MERN Stack');
 });
 
+// Route to save a new book 
+
 app.post('/books', async (request, response) => {
     try {
         if (
@@ -37,6 +39,69 @@ app.post('/books', async (request, response) => {
         response.status(500).send({ message: error.message });
     }
 });
+
+// Route to get All Books from the database
+
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+// Route to get one book from the database by id
+
+app.get('/books/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+// Route to update a book
+
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found'});
+        }
+
+        return response.status(200).json({ message: 'Book updated successfully'});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+    }
+);
+
 
 mongoose
     .connect(mongoDBURL)
